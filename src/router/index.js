1dbc880b5610211d87c9b5f9b5f2e3860982436c
Marcos Correa <>
2022-08-16 23:10:45 -0300

    adding confirmation vue structure

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ContractCode from '../views/ContractCode.vue'
 import Installments from '../views/Installments.vue'
 import PersonalData from '../views/PersonalData.vue'
-// import PaymentData from '../views/PaymentData.vue'
-// import PaymentFinished from '../views/PaymentFinished.vue'
+import Confirmation from '../views/Confirmation.vue'
 
 const routes = [
     {
@@ -19,14 +18,16 @@ const routes = [
         path: '/dados-pessoais',
         name: 'Dados Pessoais',
         component: PersonalData
-    },{
-        path: '/dados-pagamento',
-        name: 'Dados do Pagamento',
-        component: () => PaymentData
-    },{
+    },
+    // {
+    //     path: '/dados-pagamento',
+    //     name: 'Dados do Pagamento',
+    //     component: PaymentData
+    // },
+    {
         path: '/confirmacao',
         name: 'Pagamento Enviado',
-        component: () => PaymentFinished
+        component: Confirmation
     },
 ]
 
---
+++ src/views/Confirmation.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col col-sm-12 col-md-12">
+                <div class="breadcrumb">
+                    <!-- precisa atualizar breadcrumb para a pagina de confirmacao -->
+                    <img src="../assets/personal-data-timeline.svg" alt="Breadcrumb Dados Pessoais" />
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <!-- colocar layout de confirmacao  -->
+        </div>
+        <div class="row">
+            <!-- colocar botoes de acao  -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+.title {
+    text-align: center;
+}
+</style>
